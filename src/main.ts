14876fc4a246e567fe44575557b3c890a9544e8e
Ornel Kevin Piam Talom <ornel.kevin.piam.talom@mni.thm.de>
2023-10-06 00:06:01 +0200

    Bug: Set up CORS middleware and configure it to allow requests from your Angular application's origin

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api/for/le')
   app.useGlobalPipes(new ValidatePipe());
 
-  // Set up CORS
-  app.enableCors();
+  // Configure CORS to allow requests from your Angular app's origin
+  app.enableCors({
+    origin: 'https://kimpa-africa-online-shop.onrender.com', // Replace with your Angular app's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable sending cookies in cross-origin requests (if needed)
+  });
 
   // Increase the payload size limit to 10MB
   app.use(express.json({ limit: '10mb' }));
